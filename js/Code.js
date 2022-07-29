b16732b49dcef04bb2250b21a1d3ed0715537715
karri sruthi <87524555+Sruthi2299@users.noreply.github.com>
2022-07-29 07:02:44 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "ADA LOVELACE";
+const accessCode2 = "SPACE WAR";
+const accessCode3 = "A MOTH";
+
+function clues() {
+    
+    fill("BLACK")
+    textSize(15)
+    text("L A O D E L C A V E A", 100,50)
+    fill("WHITE")
+    text("Hint: Who invented Computer Programing ?", 100,70)
+
+    fill("BLACK")
+    textSize(15)
+    text("S W R A C A E P", 700,150)
+    fill("WHITE")
+    text("Hint: First programmed game in the world !! ", 700,170)
+
+    fill("BLACK")
+    textSize(15)
+    text("M H T A O", 100,250)
+    fill("WHITE")
+    text("Hint: The world's first computer bug !!", 100,270)
+
+}
